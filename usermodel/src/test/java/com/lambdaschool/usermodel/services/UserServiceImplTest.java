9ed8cdd1869059dd750ac8a6429a549ef2560015
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-07-01 12:06:02 +0800

    Create unit test file for User service

---
+++ usermodel/src/test/java/com/lambdaschool/usermodel/services/UserServiceImplTest.java
@@ -0,0 +1,58 @@
+package com.lambdaschool.usermodel.services;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserServiceImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void findUserById() {
+    }
+
+    @Test
+    public void findByNameContaining() {
+    }
+
+    @Test
+    public void findAll() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void findByName() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void getCountUserEmails() {
+    }
+
+    @Test
+    public void deleteUserRole() {
+    }
+
+    @Test
+    public void addUserRole() {
+    }
+}
