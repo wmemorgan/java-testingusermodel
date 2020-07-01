89d039d212abba6e68d07a795f419c1123b824cd
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-07-01 12:06:15 +0800

    Create unit test file for User controller

---
+++ usermodel/src/test/java/com/lambdaschool/usermodel/controllers/UserControllerTest.java
@@ -0,0 +1,62 @@
+package com.lambdaschool.usermodel.controllers;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void listAllUsers() {
+    }
+
+    @Test
+    public void getUserById() {
+    }
+
+    @Test
+    public void getUserByName() {
+    }
+
+    @Test
+    public void getUserLikeName() {
+    }
+
+    @Test
+    public void addNewUser() {
+    }
+
+    @Test
+    public void updateFullUser() {
+    }
+
+    @Test
+    public void updateUser() {
+    }
+
+    @Test
+    public void deleteUserById() {
+    }
+
+    @Test
+    public void getNumUserEmails() {
+    }
+
+    @Test
+    public void deleteUserRoleByIds() {
+    }
+
+    @Test
+    public void postUserRoleByIds() {
+    }
+}
