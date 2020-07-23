2182cd158a4e94485fe6427a95d50e931b76c005
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-07-23 12:46:34 +0800

    Remove unused imports

--- usermodel/src/test/java/com/lambdaschool/usermodel/services/UserServiceImplTest.java
+++ usermodel/src/test/java/com/lambdaschool/usermodel/services/UserServiceImplTest.java
@@ -15,8 +15,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
